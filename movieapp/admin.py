5cc6075712d6d3cc271ae3fde96adb6820563a2a
Humsbel Jao <humsbel_obregon12@hotmail.com>
2020-12-29 11:10:53 -0500

    Customize dashboard

--- movieapp/admin.py
+++ movieapp/admin.py
@@ -3,4 +3,8 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register(Movie)
+class MyMovie(admin.ModelAdmin):
+    list_display = ('name', 'description', 'year', 'star', 'show')
+    list_filter = ('star', 'show')
+    
+admin.site.register(Movie, MyMovie)
